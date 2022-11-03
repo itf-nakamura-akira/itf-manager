46699540748e59a09fda461e89497fcb3638fb8d
itf-nakamura-akira <nakamura.akira@itfllc.co.jp>
2022-11-03 05:20:47 +0000

    エラーハンドリングの実装

---
+++ api-gateway/src/middlewares/errorHandler.ts
@@ -0,0 +1,36 @@
+import { Context, HttpError, isHttpError, Status } from 'oak/mod.ts';
+
+/**
+ * 全体の例外ハンドラー
+ * 例外を投げる側の例：
+ *  throw createHttpError(Status.Unauthorized, '認証処理に失敗しました。アカウントかパスワードを確認してください。');
+ *
+ * @param ctx コンテキスト
+ * @param next 次のミドルウェア処理
+ */
+export async function errorHandler(ctx: Context, next: () => Promise<unknown>) {
+    try {
+        await next();
+    } catch (err) {
+        if (isHttpError(err)) {
+            const error: HttpError = err;
+
+            ctx.response.status = error.status;
+
+            switch (error.status) {
+                case Status.NotFound:
+                    ctx.response.body = 'リクエストされたデータが見つかりませんでした。';
+
+                    break;
+
+                default:
+                    ctx.response.body = error.message;
+
+                    break;
+            }
+        } else {
+            ctx.response.status = Status.InternalServerError;
+            ctx.response.body = 'サーバーで障害が発生しました。時間が経っても解決しない場合は、管理者にお問い合わせください。';
+        }
+    }
+}
--- api-gateway/src/server.ts
+++ api-gateway/src/server.ts
@@ -1,17 +1,25 @@
 import { Application, Context } from 'oak/mod.ts';
+import { errorHandler } from "./middlewares/errorHandler.ts";
 
 const app = new Application();
 
-app.use((ctx: Context, next) => {
+app.use(errorHandler);
+app.use(async (_: Context, next) => {
 	console.log(`1: ${new Date()}`);
-	next();
+	await next();
 });
-app.use((ctx: Context, next) => {
+app.use(async (ctx: Context, next) => {
 	console.log(`2: ${new Date()}`);
 	ctx.response.body = {
 		data: 'Hello World!',
 	};
-	next();
+
+	await next();
+});
+
+// サーバー起動時のイベント
+app.addEventListener('listen', ({ hostname, port, secure }) => {
+	console.log(`Listening on: ${secure ? 'https://' : 'http://'}${hostname ?? 'localhost'}:${port}`);
 });
 
 await app.listen({ port: 8000 });
